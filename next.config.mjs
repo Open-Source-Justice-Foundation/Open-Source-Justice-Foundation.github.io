4cf41e1cafab74651074fdfae93f985d3bfc9ae2
Bri <brianjcarroll0@gmail.com>
2024-05-11 17:51:38 -0400

    Update nextjs config to static output for github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export", // <=== enables static exports
+  reactStrictMode: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
