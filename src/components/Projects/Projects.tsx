0fe8bc2226b693fe09af4155a8bdc68f2bdb3783
Bri <brianjcarroll0@gmail.com>
2024-10-23 12:10:09 -0400

    FFK

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,4 +1,4 @@
-import { Keystache, Fundsolvr, Duck, Github } from "../icons";
+import { Keystache, Fundsolvr, Duck, Github, FFK } from "../icons";
 import SectionTitle from "../shared/SectionTitle";
 export default function Projects() {
   return (
@@ -9,8 +9,31 @@ export default function Projects() {
       />
       <div className="grid grid-cols-1 md:grid-cols-3 gap-12">
         <div className="flex flex-col gap-4">
+          <FFK />
+          <h4 className="font-medium font-hubot text-xl text-title mt-2 md:mt-0">
+            FFK
+          </h4>
+          <p className="font-hubot text-md text-heading">
+            The Foundation Formation Kit (FFK) is a set of tools provided by
+            OSJF to increase access to tax-advantaged, non-profit structures for
+            open-source projects of all sizes. With easy to use forms,
+            instructions, and even a webapp, the FFK will transform the
+            open-source industry into a robust ecosystem of non-profits,
+            accelerating capital allocation and supporting the development of
+            open-source technologies and protocols.
+          </p>
+          <div className="flex flex-row gap-4 mt-auto">
+            <a
+              href="https://github.com/Open-Source-Justice-Foundation/foundation-formation-kit"
+              target="_blank"
+            >
+              <Github width="32" height="33" />
+            </a>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4">
           <a href="https://fundsolvr.com" target="_blank">
-           <Fundsolvr />
+            <Fundsolvr />
           </a>
           <h4 className="font-medium font-hubot text-xl text-title mt-2 md:mt-0">
             Fundsolvr
@@ -23,9 +46,11 @@ export default function Projects() {
             resolution tools provide assurances that further increase market
             efficiencies.
           </p>
-          <div className="flex flex-row gap-4">
-            {/* <Duck /> */}
-            <a href="https://github.com/Open-Source-Justice-Foundation/Fundsolvr" target="_blank">
+          <div className="flex flex-row gap-4 mt-auto">
+            <a
+              href="https://github.com/Open-Source-Justice-Foundation/Fundsolvr"
+              target="_blank"
+            >
               <Github width="32" height="33" />
             </a>
           </div>
@@ -44,7 +69,6 @@ export default function Projects() {
             seamless OAUTH-style login experience across NOSTR-applications.
           </p>
           <div className="flex flex-row gap-4 mt-auto">
-            {/* <Duck /> */}
             <a href="https://github.com/nodetec/keystache" target="_blank">
               <Github width="32" height="33" />
             </a>
